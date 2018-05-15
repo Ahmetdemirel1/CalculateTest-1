a0bdb210566e0e5394b8abaff9ebb0eee750bb85
Ali Aktolun <ali.aktolun@sahabt.com>
2018-05-15 15:13:56 +0300

    constructor add

--- src/main/java/com/testinium/web/process/CalculateProcessImpl.java
+++ src/main/java/com/testinium/web/process/CalculateProcessImpl.java
@@ -4,6 +4,16 @@ import com.testinium.web.model.Number;
 
 public class CalculateProcessImpl implements CalculateProcess {
 
+  public CalculateProcessImpl() {
+    System.out.println("Calculate process created");
+  }
+
+  @Override
+  protected void finalize() throws Throwable {
+    System.out.println("Calculate process remove");
+    super.finalize();
+  }
+
   @Override
   public int sum(Number numberOne, Number numberTwo) {
     return numberOne.getNumber() + numberTwo.getNumber();
--- src/test/java/com/testinium/web/process/CalculateProcessImplTest.java
+++ src/test/java/com/testinium/web/process/CalculateProcessImplTest.java
@@ -10,20 +10,28 @@ import org.junit.Test;
 
 public class CalculateProcessImplTest {
 
-  CalculateProcess calculateProcess = new CalculateProcessImpl();
+  CalculateProcess calculateProcess;
 
   @Before
   public void setUp() throws Exception {
     System.out.println("Before test method");
+    calculateProcess = new CalculateProcessImpl();
   }
 
   @After
   public void tearDown() throws Exception {
     System.out.println("After test method");
+    calculateProcess = null;
   }
 
   @Test
   public void sum() {
+    int result = calculateProcess.sum(new Number(3), new Number(5));
+    Assert.assertEquals(8, result);
+  }
+
+  @Test
+  public void sum2() {
     int result = calculateProcess.sum(new Number(3), new Number(5));
     Assert.assertEquals(8, result);
   }
